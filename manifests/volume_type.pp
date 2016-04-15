3fdf4ed087549c388628cf48135857440f5241b6
Andrey Pavlov <andrey-mp@yandex.ru>
2016-04-15 14:59:14 +0300

    update 2015.1.1.19662.diff

--- manifests/volume_type.pp
+++ manifests/volume_type.pp
@@ -1,10 +1,10 @@
 # Define for creation of volume types for ScaleIO
 #
 define scaleio_openstack::volume_type(
-  $name,                        # name of volume type to be created
-  $protectio_domain,            # name of protection domain to tie with volume type
-  $storage_pool,                # name of storage pool to tie with volume type
   $ensure           = present,  #
+  $name,                        # name of volume type to be created
+  $protection_domain,           # name of protection domain to tie with volume type
+  $storage_pool,                # name of storage pool to tie with volume type
   $os_user          = undef,    # OpenStack user name
   $os_password      = undef,    # OpenStack user password
   $os_tenant        = undef,    # OpenStack tenant name
@@ -27,13 +27,13 @@ define scaleio_openstack::volume_type(
     }
   }  
   if $ensure == present {
-    exec {"ScaleIO Cinder Volume Type ${name} ${ensure}, pd: ${protectio_domain}, sp: ${storage_pool}":
+    exec {"ScaleIO Cinder Volume Type ${name} ${ensure}, pd: ${protection_domain}, sp: ${storage_pool}":
       command     => "bash -c '${source_opts} cinder type-create ${name}'",
       path        => ['/usr/bin', '/bin'],
       unless      => "bash -c '${source_opts} cinder type-list | grep -q \"${name}\"'",
     } ->
-    exec {"Properties for ScaleIO Cinder Volume Type ${name}, pd: ${protectio_domain}, sp: ${storage_pool}":
-      command => "bash -c '${source_opts} cinder type-key ${name} set sio:pd_name=${protectio_domain} sio:provisioning=thin sio:sp_name=${storage_pool}'",
+    exec {"Properties for ScaleIO Cinder Volume Type ${name}, pd: ${protection_domain}, sp: ${storage_pool}":
+      command => "bash -c '${source_opts} cinder type-key ${name} set sio:pd_name=${protection_domain} sio:provisioning=thin sio:sp_name=${storage_pool}'",
       path    => ['/usr/bin', '/bin'],
       onlyif  => "bash -c '${source_opts} cinder type-list | grep -q \"${name}\"'",
     }    
