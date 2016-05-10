61b8cb14c14d8d7364fcbb1dc753f4e9c7645633
alexey-mr <alexey.morlang@gmail.com>
2016-05-10 23:17:32 +0300

    Revert nova OS facts back, move dangerous code into setcode scope to avoid broken puppet in case of errors in config files.

--- lib/facter/nova_info.rb
+++ lib/facter/nova_info.rb
@@ -19,56 +19,57 @@ Facter.add(:nova_version) do
   end
 end
 
-#if Facter.value(:nova_version)
-#  nova_props = {
-#    'nova_username'       => 'admin_user',
-#    'nova_password'       => 'admin_password',
-#    'nova_tenant_name'    => 'admin_tenant_name',
-#  }
-#  nova_config_file = '/etc/nova/nova.conf'
-#  if File.exist?(nova_config_file)
-#    config = Puppet::Util::IniConfig::File.new
-#    config.read(nova_config_file)
-#    section = config['keystone_authtoken']
-#    if section
-#      nova_props.each do |key, value|
-#        Facter.add(key) do
-#          setcode do
-#            section[value]
-#          end
-#        end
-#      end
-#      Facter.add(:nova_auth_uri) do
-#        setcode do
-#          uri = nil
-#          if section['auth_uri']
-#            uri = section['auth_uri']
-#          elsif section['identity_uri']
-#            uri = section['identity_uri']
-#          else
-#            uri = "%s://%s:%s" % [section['auth_protocol'], section['auth_host'], section['auth_port']]
-#          end
-#          api_version = section['auth_version']
-#          if not api_version
-#            resp  = Facter::Util::Resolution.exec( "curl -k --basic --connect-timeout 10 %s" % uri)
-#            if resp
-#              spec = JSON.parse(resp)
-#              if spec and spec['versions'] and spec['versions']['values']
-#                version = 'v2.0'
-#                spec['versions']['values'].each do |val|
-#                  if version == val['id']
-#                    version = val['id']
-#                    uri = val['links'][0]['href'] unless val['links'].count() == 0
-#                  end
-#                end
-#              end
-#            end
-#          else
-#            uri = "%s/%s" % [uri, api_version]
-#          end
-#          uri
-#        end
-#      end
-#    end
-#  end
-#end
+if Facter.value(:nova_version)
+  nova_props = {
+    'nova_username'       => 'admin_user',
+    'nova_password'       => 'admin_password',
+    'nova_tenant_name'    => 'admin_tenant_name',
+  }
+  nova_config_file = '/etc/nova/nova.conf'
+  if File.exist?(nova_config_file)
+    nova_props.each do |key, value|
+      Facter.add(key) do
+        setcode do
+          config = Puppet::Util::IniConfig::File.new
+          config.read(nova_config_file)
+          section = config['keystone_authtoken']
+          section[value].strip()
+        end
+      end
+    end
+    Facter.add(:nova_auth_uri) do
+      setcode do
+        config = Puppet::Util::IniConfig::File.new
+        config.read(nova_config_file)
+        section = config['keystone_authtoken']
+        uri = nil
+        if section['auth_uri']
+          uri = section['auth_uri']
+        elsif section['identity_uri']
+          uri = section['identity_uri']
+        else
+          uri = "%s://%s:%s" % [section['auth_protocol'], section['auth_host'], section['auth_port']]
+        end
+        api_version = section['auth_version']
+        if not api_version
+          resp  = Facter::Util::Resolution.exec( "curl -k --basic --connect-timeout 10 %s" % uri)
+          if resp
+            spec = JSON.parse(resp)
+            if spec and spec['versions'] and spec['versions']['values']
+              version = 'v2.0'
+              spec['versions']['values'].each do |val|
+                if version == val['id']
+                  version = val['id']
+                  uri = val['links'][0]['href'] unless val['links'].count() == 0
+                end
+              end
+            end
+          end
+        else
+          uri = "%s/%s" % [uri, api_version]
+        end
+        uri.strip()
+      end
+    end
+  end
+end
