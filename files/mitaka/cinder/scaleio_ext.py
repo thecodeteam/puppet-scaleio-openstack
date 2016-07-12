7517e258ef38ce112f1a5c6bccacacdc509fee3c
Andrey Pavlov <andrey-mp@yandex.ru>
2016-07-12 13:41:43 +0300

    fix cinder/scaleio_ext.py for Mitaka

--- files/mitaka/cinder/scaleio_ext.py
+++ files/mitaka/cinder/scaleio_ext.py
@@ -25,13 +25,19 @@ from cinder.volume.drivers.emc import scaleio
 
 LOG = logging.getLogger(__name__)
 
-IOPS_PER_GB_KEY = 'sio:iops_per_gb'
+IOPS_PER_GB = 'sio:iops_per_gb'
 BANDWIDTH_PER_GB = 'sio:bandwidth_per_gb'
+QOS_IOPS_PER_GB = 'maxIOPSperGB'
+QOS_BANDWIDTH_PER_GB = 'maxBWSperGB'
 
+MIN_BWS_SCALING_SIZE = 128
 
 class ScaleIODriver(scaleio.ScaleIODriver):
 
     def __init__(self, *args, **kwargs):
+        scaleio_qos_keys = (scaleio.QOS_IOPS_LIMIT_KEY,
+                            scaleio.QOS_BANDWIDTH_LIMIT,
+                            QOS_IOPS_PER_GB, QOS_BANDWIDTH_PER_GB)
         super(ScaleIODriver, self).__init__(*args, **kwargs)
         self.provisioning_type = (
             'thin'
@@ -54,13 +60,15 @@ class ScaleIODriver(scaleio.ScaleIODriver):
 
     def _get_bandwidth_limit(self, size, storage_type):
         try:
-            max_bandwidth = self._find_bandwidth_limit(storage_type)
-            max_bandwidth = (
-                None if max_bandwidth is None else
-                six.text_type(self._round_to_num_gran(int(max_bandwidth),
-                                                      1024)))
+            max_bandwidth = self._find_limit(storage_type,
+                scaleio.QOS_BANDWIDTH_LIMIT, scaleio.BANDWIDTH_LIMIT)
+            if max_bandwidth is not None:
+                max_bandwidth = (self._round_to_num_gran(int(max_bandwidth),
+                                                         units.Ki))
+                max_bandwidth = six.text_type(max_bandwidth)
             LOG.info(_LI("max bandwidth is: %s"), max_bandwidth)
-            bw_per_gb = storage_type.get(BANDWIDTH_PER_GB)
+            bw_per_gb = self._find_limit(storage_type,
+                QOS_BANDWIDTH_PER_GB, BANDWIDTH_PER_GB)
             LOG.info(_LI("bandwidth per gb is: %s"), bw_per_gb)
             if bw_per_gb is None:
                 return max_bandwidth
@@ -68,31 +76,37 @@ class ScaleIODriver(scaleio.ScaleIODriver):
             # and BWS limitation is in 1024 KBs granularity, we need to make
             # sure that scaled_bw_limit is in 128 granularity.
             scaled_bw_limit = (size *
-                               self._round_to_num_gran(int(bw_per_gb), 128))
+                               self._round_to_num_gran(int(bw_per_gb),
+                                                       MIN_BWS_SCALING_SIZE))
             if max_bandwidth is None or scaled_bw_limit < int(max_bandwidth):
                 return six.text_type(scaled_bw_limit)
             else:
                 return max_bandwidth
         except ValueError:
             msg = _("None numeric BWS QoS limitation")
-            raise exception.InvalidInput(data=msg)
+            raise exception.InvalidInput(reason=msg)
 
     def _get_iops_limit(self, size, storage_type):
-        max_iops = self._find_iops_limit(storage_type)
+        max_iops = self._find_limit(storage_type,
+            scaleio.QOS_IOPS_LIMIT_KEY, scaleio.IOPS_LIMIT_KEY)
         LOG.info(_LI("max iops is: %s"), max_iops)
-        iops_per_gb = storage_type.get(IOPS_PER_GB_KEY)
+        iops_per_gb = self._find_limit(storage_type,
+            QOS_IOPS_PER_GB, IOPS_PER_GB)
         LOG.info(_LI("iops per gb is: %s"), iops_per_gb)
         try:
             if iops_per_gb is None:
-                return max_iops
+                if max_iops is not None:
+                    return six.text_type(max_iops)
+                else:
+                    return None
             scaled_iops_limit = size * int(iops_per_gb)
             if max_iops is None or scaled_iops_limit < int(max_iops):
                 return six.text_type(scaled_iops_limit)
             else:
-                return max_iops
+                return six.text_type(max_iops)
         except ValueError:
             msg = _("None numeric IOPS QoS limitation")
-            raise exception.InvalidInput(data=msg)
+            raise exception.InvalidInput(reason=msg)
 
     def _round_to_num_gran(self, size, num=8):
         if size % num == 0:
