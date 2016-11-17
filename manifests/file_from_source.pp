65dad81fe0be7faae35ee0440fa69122e227ff4e
alexey-mr <alexey.morlang@gmail.com>
2016-11-17 13:17:27 +0300

    Move glance configuration logic from fuel to reuse from tripleo

--- manifests/file_from_source.pp
+++ manifests/file_from_source.pp
@@ -3,13 +3,17 @@ define scaleio_openstack::file_from_source(
   $dir,
   $file_name,
   $src_dir,
+  $dst_file_name  = $file_name,
+  $mode           = '0644',
+  $owner          = 'root',
+  $group          = 'root',
 )
 {
-  file { "${dir}/${file_name}":
+  file { "${dir}/${dst_file_name}":
     ensure => $ensure,
     source => "puppet:///modules/scaleio_openstack/${src_dir}/${file_name}",
-    mode   => '0644',
-    owner  => 'root',
-    group  => 'root',
+    mode   => $mode,
+    owner  => $owner,
+    group  => $group,
   }
 }
--- manifests/glance.pp
+++ manifests/glance.pp
@@ -1,5 +1,34 @@
+# Class to configure glance to use ScaleIO for images (via Cinder backend).
+
+
+# Helping define for internal use
+define scaleio_openstack::glance_config(
+  $config_file,
+) {
+  if $config_file and $config_file != '' {
+    ini_subsetting { "${config_file}: stores":
+      ensure               => 'present',
+      path                 => $config_file,
+      section              => 'glance_store',
+      setting              => 'stores',
+      subsetting           => 'glance.store.cinder.Store',
+      subsetting_separator => ',',
+    } ->
+    ini_setting { "${config_file}: default_store":
+      ensure  => 'present',
+      path    => $config_file,
+      section => 'glance_store',
+      setting => 'default_store',
+      value   => 'cinder',
+    }
+  }
+}
+
+
 class scaleio_openstack::glance (
-  $ensure = present,    # could be present or absent
+  $ensure         = present,                          # could be present or absent
+  $glance_config  = '/etc/glance/glance-api.conf',    # if empty or undef the config actions be skipped
+  $glare_config   = '/etc/glance/glance-glare.conf',
 )
 {
   notify {'Configure Glance to use ScaleIO cluster via Cinder': }
@@ -8,20 +37,55 @@ class scaleio_openstack::glance (
     warning('Glance is not installed on this node')
   }
   else {
-    $version_str = split($::glance_version, '-')
-    $version = $version_str[0]
-    $version_array = split($version, '\.')
-
-    if $version_array[0] >= '12' {
-      notify { "Detected glance version ${version}": }
-
-      scaleio_openstack::scaleio_filter_file { 'glance filter file':
-        ensure  => $ensure,
-        service => 'glance',
+    $glance_config_provided = $glance_config and $glance_config != ''
+    $glare_config_provided = $glare_config and $glare_config != ''
+    if $glance_config_provided or $glare_config_provided {
+      $version_str = split($::glance_version, '-')
+      $version = $version_str[0]
+      $version_array = split($version, '\.')
+      if $version_array[0] >= '12' {
+        notify { "Detected glance version ${version}": }
+        $glance_services = $::osfamily ? {
+          'RedHat' => ['openstack-glance-api', 'openstack-glance-registry', 'openstack-glance-glare'],
+          'Debian' => ['glance-api', 'glance-registry', 'glance-glare'],
+        }
+        package { ['python-cinderclient', 'python-os-brick', 'python-oslo.rootwrap']:
+          ensure => 'present',
+        } ->
+        scaleio_openstack::scaleio_filter_file { 'glance filter file':
+          ensure  => $ensure,
+          service => 'glance',
+        } ->
+        scaleio_openstack::file_from_source { 'glance_rootwrap':
+          ensure        => $ensure,
+          dir           => '/etc/glance',
+          file_name     => 'glance_rootwrap.conf',
+          src_dir       => '.',
+          dst_file_name => 'rootwrap.conf',
+          mode          => '0755',
+        } ->
+        scaleio_openstack::file_from_source { 'glance_sudoers':
+          ensure    => $ensure,
+          dir       => '/etc/sudoers.d',
+          file_name => 'glance_sudoers',
+          src_dir   => '.',
+        } ->
+        glance_config { "glance config: ${glance_config}":
+          config_file => $glance_config,
+        } ->
+        glance_config { "glare config: ${glare_config}":
+          config_file => $glare_config,
+        } ~>
+        service { $glance_services:
+          ensure => running,
+          enable => true,
+        }
       }
-    }
-    else {
-      fail("Version ${version} of python-glance isn't supported.")
+      else {
+        fail("Version ${version} of python-glance isn't supported.")
+      }
+    } else {
+      notify { 'Skip glance configuration because of no config file provided': }
     }
   }
 }
